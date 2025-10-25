fc0a305503eeb1c971e77e8df3d3418db3eb61aa
mohammedshanid <123456789+odin@users.noreply.github.com>
2025-10-25 08:01:04 +0000

    updated base_url

--- client/services/base_url.js
+++ client/services/base_url.js
@@ -1,2 +1,3 @@
-export const base_url = "http://localhost:3000/api";
+// export const base_url = "http://localhost:3000/api";
+export const base_url = "https://student-mangement-sq78.onrender.com/api"
 
